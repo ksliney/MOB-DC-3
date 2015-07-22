859af0960e162bd956024eecefa18c44c6119df0
ksliney <franmar@FRANMARs-MacBook-Air.local>
2015-07-22 17:51:19 -0400

    calc frontend

--- Homework/Midterm/Calculator/Calculator/ViewController.swift
+++ Homework/Midterm/Calculator/Calculator/ViewController.swift
@@ -1,21 +1,18 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by FRANMAR on 7/20/15.
-//  Copyright (c) 2015 FRANMAR. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var calcDisplay: UILabel!
-    
-
-    
-    @IBAction func addDigit(sender: UIButton) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+        
+        func buttonTap(theButton: UIButton) {
+            println(theButton.titleLabel!.text)
+        }
+    
 }
+
 
